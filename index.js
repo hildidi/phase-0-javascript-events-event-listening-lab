28f922263b1e4d19a348b268e48e485740391ede
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-09 00:22:03 -0500

    commit tree 5fe047765acdfcd35c2007db7539000e2f4b6d29

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function    addingEventListener() {
-const input = document.getElementById('input');
+// function addingEventListener() {
+// const input = document.getElementById('input');
 
-function clickAlert() {
-  alert('I was clicked!');
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+// input.addEventListener('click', clickAlert);
+// }
+// addingEventListener();
+// clickAlert;
+
+
+function addingEventListener(){
+  const input = document.querySelector('input');
+  const clickAlert = alert('I was clicked!');
+
+input.addEventListener('click',clickAlert);
 }
-input.addEventListener('click', clickAlert);
-}
-addingEventListener();
-clickAlert;
